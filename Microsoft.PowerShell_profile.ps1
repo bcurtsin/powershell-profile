ff7585ead421ffa8aa5b82da34c85ad154e5a705
Brian Curtsinger <bcurtsin@quiktrip.com>
2024-06-10 08:53:03 -0500

    load local zen.toml

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -239,7 +239,8 @@ Set-PSReadLineOption -Colors @{
 Set-PSReadLineOption -EditMode Emacs
 
 ## Final Line to set prompt
-oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/catppuccin_mocha.omp.json | Invoke-Expression
+#oh-my-posh init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/catppuccin_mocha.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config $env:home/.config/ohmyposh/zen.toml | Invoke-Expression
 
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
     Invoke-Expression (& { (zoxide init powershell | Out-String) })
